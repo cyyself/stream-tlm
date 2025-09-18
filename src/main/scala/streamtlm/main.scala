789f5018d6d397556f14188b3fa3ef57916cf409
Yangyu Chen <cyy@cyyself.name>
2025-09-18 18:14:05 +0800

    main.scala: align port parms with rocket-chip cyysoc

--- src/main/scala/streamtlm/main.scala
+++ src/main/scala/streamtlm/main.scala
@@ -9,9 +9,9 @@ object StreamTLM extends App {
     val outDir = Paths.get("build/rtl")
     val params = new StreamTLMParams(
         edges = Seq(
-            new AXI4EdgeParams  (name = "MMIO", axisAddr = 0, isMaster = false, interface = new AXI4Params(addrWidth = 64, dataWidth = 64)),
-            new AXI4EdgeParams  (name = "MEM",  axisAddr = 1, isMaster = false, interface = new AXI4Params(addrWidth = 64, dataWidth = 64)),
-            new AXI4EdgeParams  (name = "DMA",  axisAddr = 2, isMaster = true,  interface = new AXI4Params(addrWidth = 64, dataWidth = 64)),
+            new AXI4EdgeParams  (name = "MMIO", axisAddr = 0, isMaster = false, interface = new AXI4Params(addrWidth = 32, dataWidth = 64)),
+            new AXI4EdgeParams  (name = "MEM",  axisAddr = 1, isMaster = false, interface = new AXI4Params(addrWidth = 32, dataWidth = 64)),
+            new AXI4EdgeParams  (name = "DMA",  axisAddr = 2, isMaster = true,  interface = new AXI4Params(addrWidth = 32, dataWidth = 64, idWidth = 8)),
             new IntBusEdgeParams(name = "INT",  axisAddr = 3, width = 4)
         ),
         outDir = outDir
